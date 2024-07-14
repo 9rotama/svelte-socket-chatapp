31224100ba4de90bd19858072e4e28c828149d0c
9rotama <aoim99.sc@gmail.com>
2024-07-15 01:20:12 +0900

    chore: prettierrc変更

--- src/lib/components/ui/button/button.stories.svelte
+++ src/lib/components/ui/button/button.stories.svelte
@@ -1,15 +1,15 @@
 <script lang="ts" context="module">
-	import Button from './button.svelte';
-	import type { Meta } from '@storybook/svelte';
+	import Button from "./button.svelte";
+	import type { Meta } from "@storybook/svelte";
 
 	export const meta = {
-		title: 'button',
-		component: Button
+		title: "button",
+		component: Button,
 	} satisfies Meta<Button>;
 </script>
 
 <script lang="ts">
-	import { Template, Story } from '@storybook/addon-svelte-csf';
+	import { Template, Story } from "@storybook/addon-svelte-csf";
 </script>
 
 <Template let:args>
