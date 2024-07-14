76505af16cc12818fe4c96bd46a862a940ca068c
9rotama <aoim99.sc@gmail.com>
2024-07-15 00:50:56 +0900

    shadcnのbutton追加

---
+++ src/lib/components/ui/button/button.stories.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" context="module">
+	import Button from './button.svelte';
+	import type { Meta } from '@storybook/svelte';
+
+	export const meta = {
+		title: 'button',
+		component: Button
+	} satisfies Meta<Button>;
+</script>
+
+<script lang="ts">
+	import { Template, Story } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+	<Button {...args}>button</Button>
+</Template>
+
+<Story name="Default" />
