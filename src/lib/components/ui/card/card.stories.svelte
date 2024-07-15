5a8da40402086065626de6885e62845a2d309ace
9rotama <aoim99.sc@gmail.com>
2024-07-15 22:57:12 +0900

    feat: cardのstory追加

---
+++ src/lib/components/ui/card/card.stories.svelte
@@ -0,0 +1,31 @@
+<script lang="ts" context="module">
+  import Card from "./card.svelte";
+  import type { Meta } from "@storybook/svelte";
+
+  export const meta = {
+    title: "card",
+    component: Card,
+  } satisfies Meta<Card>;
+</script>
+
+<script lang="ts">
+  import { Story } from "@storybook/addon-svelte-csf";
+  import CardTitle from "./card-title.svelte";
+  import CardContent from "./card-content.svelte";
+  import CardHeader from "./card-header.svelte";
+  import CardDescription from "./card-description.svelte";
+  import CardFooter from "./card-footer.svelte";
+</script>
+
+<Story name="Default">
+  <Card>
+    <CardContent>
+      <CardHeader>
+        <CardTitle>card title</CardTitle>
+      </CardHeader>
+      <CardFooter>
+        <CardDescription>card description</CardDescription>
+      </CardFooter>
+    </CardContent>
+  </Card>
+</Story>
