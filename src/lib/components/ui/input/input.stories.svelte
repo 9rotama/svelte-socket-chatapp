9ee70abd5617078b3163232b07726a9360693ba6
9rotama <aoim99.sc@gmail.com>
2024-07-15 23:26:14 +0900

    feat: input, labelのstory追加

---
+++ src/lib/components/ui/input/input.stories.svelte
@@ -0,0 +1,15 @@
+<script lang="ts" context="module">
+  import Input from "./input.svelte";
+  import type { Meta } from "@storybook/svelte";
+
+  export const meta = {
+    title: "input",
+    component: Input,
+  } satisfies Meta<Input>;
+</script>
+
+<script lang="ts">
+  import { Story } from "@storybook/addon-svelte-csf";
+</script>
+
+<Story name="Default"><Input type="text" /></Story>
---
+++ src/lib/components/ui/label/label.stories.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" context="module">
+  import Label from "./label.svelte";
+  import type { Meta } from "@storybook/svelte";
+
+  export const meta = {
+    title: "label",
+    component: Label,
+  } satisfies Meta<Label>;
+</script>
+
+<script lang="ts">
+  import { Story } from "@storybook/addon-svelte-csf";
+  import Input from "../input/input.svelte";
+</script>
+
+<Story name="Default">
+  <Label>label</Label>
+  <Input type="text" />
+</Story>
