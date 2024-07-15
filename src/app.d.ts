40eaebbb7a504a5d64f44271c5f0ef7debabcba5
9rotama <aoim99.sc@gmail.com>
2024-07-16 02:21:14 +0900

    feat: supabase clientをssr用に乗り換え

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,18 @@
 declare global {
   namespace App {
     // interface Error {}
-    // interface Locals {}
+    interface Locals {
+      supabase: SupabaseClient;
+      safeGetSession: () => Promise<{
+        session: Session | null;
+        user: User | null;
+      }>;
+      session: Session | null;
+      user: User | null;
+    }
+    interface PageData {
+      session: Session | null;
+    }
     // interface PageData {}
     // interface PageState {}
     interface Platform {
---
+++ src/routes/+layout.ts
@@ -0,0 +1,49 @@
+import {
+  createBrowserClient,
+  createServerClient,
+  isBrowser,
+} from "@supabase/ssr";
+import type { LayoutLoad } from "./$types";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+export const load: LayoutLoad = async ({ data, depends, fetch }) => {
+  /**
+   * Declare a dependency so the layout can be invalidated, for example, on
+   * session refresh.
+   */
+  depends("supabase:auth");
+
+  const supabase = isBrowser()
+    ? createBrowserClient(supabaseUrl, supabaseKey, {
+        global: {
+          fetch,
+        },
+      })
+    : createServerClient(supabaseUrl, supabaseKey, {
+        global: {
+          fetch,
+        },
+        cookies: {
+          getAll() {
+            return data.cookies;
+          },
+        },
+      });
+
+  /**
+   * It's fine to use `getSession` here, because on the client, `getSession` is
+   * safe, and on the server, it reads `session` from the `LayoutData`, which
+   * safely checked the session using `safeGetSession`.
+   */
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return { session, supabase, user };
+};
