eef4d8769312b255474703b0046088a3a0fe2ba3
9rotama <aoim99.sc@gmail.com>
2024-07-16 00:12:25 +0900

    feat: login-form UI作成

---
+++ src/lib/components/features/login/login-form.stories.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" context="module">
+  import LoginForm from "./login-form.svelte";
+  import type { Meta } from "@storybook/svelte";
+
+  export const meta = {
+    title: "login-form",
+    component: LoginForm,
+  } satisfies Meta<LoginForm>;
+</script>
+
+<script lang="ts">
+  import { Story } from "@storybook/addon-svelte-csf";
+</script>
+
+<Story name="Default">
+  <LoginForm />
+</Story>
---
+++ src/lib/components/features/login/login-form.svelte
@@ -0,0 +1,29 @@
+<script>
+  import {
+    Card,
+    CardTitle,
+    CardHeader,
+    CardContent,
+    CardFooter,
+  } from "../../ui/card";
+  import { Label } from "../../ui/label";
+  import { Input } from "../../ui/input";
+  import { Button } from "../../ui/button";
+  import CardDescription from "../../ui/card/card-description.svelte";
+</script>
+
+<Card class="max-w-[400px]">
+  <CardHeader>
+    <CardTitle>login</CardTitle>
+    <CardDescription>login to your account</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <Label for="email">email</Label>
+    <Input type="email" />
+    <Label for="password">password</Label>
+    <Input type="password" />
+  </CardContent>
+  <CardFooter>
+    <Button class="w-full" type="submit">login</Button>
+  </CardFooter>
+</Card>
