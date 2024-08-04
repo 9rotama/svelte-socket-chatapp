28263b732ba7b10665af76c628960e3476ce4ddc
9rotama <aoim99.sc@gmail.com>
2024-08-05 01:15:45 +0900

    feat: login action実行

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,10 +1,25 @@
 import { loginFormSchema } from "@/lib/components/features/login/schema";
-import { superValidate } from "sveltekit-superforms";
+import { fail, superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
-import type { PageServerLoad } from "../$types";
+import type { Actions, PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
   return {
     form: await superValidate(zod(loginFormSchema)),
   };
+};
+
+export const actions: Actions = {
+  default: async (event) => {
+    const form = await superValidate(event, zod(loginFormSchema));
+    console.log(form.data);
+    if (!form.valid) {
+      return fail(400, {
+        form,
+      });
+    }
+    return {
+      form,
+    };
+  },
 };
