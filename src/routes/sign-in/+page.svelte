22d3cbf82a2babf9b680995bc42ef0ddae665e10
9rotama <aoim99.sc@gmail.com>
2025-01-27 02:26:56 +0900

    feat: supabase & login page

---
+++ src/routes/sign-in/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Card from '$lib/components/ui/card';
+	import { LogIn } from 'lucide-svelte';
+</script>
+
+<main class="grid h-screen w-screen place-items-center">
+	<Card.Root>
+		<Card.Header class="text-center">
+			<Card.Title>svelte-socket-chatapp</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<Button variant="outline"><LogIn />Googleアカウントでログイン</Button>
+		</Card.Content>
+	</Card.Root>
+</main>
